9eccae48ac5e2c1912f66bd75ab351b2f769ddee
suryajayantara <ptsuryajayantara01@gmail.com>
2019-09-20 14:37:34 +0800

    Update all in var , missed getch

--- Evaluasi/soal_5.c
+++ Evaluasi/soal_5.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-#include <math.h>
+
 
 void main()
 {
     int sSemua,kBesar,kSedang,kKecil,kemasan,hKemasan,sBox,totalKemasan,totalHarga;
 
+    printf("\t\t Toko Kopi Mas Athong\n");
+    printf("---------------------------------------------------------\n");
     printf("Masukan Jumlah Kemasan \t= ");
     scanf("%d",&kemasan);
 
@@ -14,9 +16,10 @@ void main()
     sBox = kemasan%20;
 
     kSedang = sBox/10;
-    sBox = kemasan%10;
+    sBox = sBox%10;
 
     kKecil = sBox / 5;
+    sBox = sBox%5;
 
     sSemua = sBox < 5 && sBox > 0  ;
 
@@ -24,12 +27,13 @@ void main()
 
     totalKemasan = (kBesar*10000) + (kSedang*7500) + (kKecil * 5000);
     totalHarga = kemasan*hKemasan + totalKemasan;
-
-    printf("Kemasan Keci \t= %d \n",kBesar);
+    printf("---------------------------------------------------------\n");
+    printf("Kemasan Besar \t= %d \n",kBesar);
     printf("Kemasan Sedang \t= %d \n",kSedang);
     printf("Kemasan Kecil \t= %d \n",kKecil);
-
-    printf("Total Bayar = %d",totalHarga);
+    printf("---------------------------------------------------------\n");
+    printf("Total Bayar = Rp . %d",totalHarga);
 
 
 }
+
